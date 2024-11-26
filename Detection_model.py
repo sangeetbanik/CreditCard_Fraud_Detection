e1cb390d798ff4aafeba274a6020ea150f6c198e
Sangeet Banik <142200832+sangeetbanik@users.noreply.github.com>
2024-11-26 13:10:54 +0530

    Update Detection_model.py

--- Detection_model.py
+++ Detection_model.py
@@ -15,3 +15,13 @@ from urllib.request import DataHandler
 
 print('Import successful')
 
+-- Importing dataset for wrangling
+df = pd.read_csv(r"C:\Users\Pratik Banik\Downloads\UM_Projects\Project_4_CreditCardFraud\transactions.csv")
+
+df['date'] = pd.to_datetime(df['date'], format='%d-%m-%Y').dt.date
+df['date'] = pd.to_datetime(df['date']) 
+df['year'] = df['date'].dt.year         # Extract year
+df['month'] = df['date'].dt.month       # Extract month
+df['day'] = df['date'].dt.day           # Extract day
+df['day_of_week'] = df['date'].dt.dayofweek  # Extract day of the week
+
