dff65e02e2acc48c938fe7b689464f2c91f6d5ad
Sangeet Banik <142200832+sangeetbanik@users.noreply.github.com>
2024-11-26 14:23:40 +0530

    Update Detection_model.py

--- Detection_model.py
+++ Detection_model.py
@@ -7,10 +7,6 @@ from sklearn.preprocessing import StandardScaler
 import seaborn as sns 
 from sklearn.ensemble import IsolationForest
 from sklearn.cluster import KMeans
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import Sequential # type: ignore
-from tensorflow.keras.layers import Dense, Dropout # type: ignore
-from sklearn.metrics import classification_report
 from urllib.request import DataHandler
 from sklearn.decomposition import PCA
 
